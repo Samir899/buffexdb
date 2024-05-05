b37f6d8376b9024223965080d4ec217dae59f8fc
Samirr <sameerkmr823@gmail.com>
2024-05-05 12:55:52 +0530

    functional Classification page adding

---
+++ src/main/java/com/buffalo/controller/FunctionalClassificationController.java
@@ -0,0 +1,13 @@
+package com.buffalo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.buffalo.service.SampleDescriptionService;
+
+@Controller
+public class FunctionalClassificationController {
+//
+//	@Autowired
+//	private FunctionalClassificationService service;
+}
